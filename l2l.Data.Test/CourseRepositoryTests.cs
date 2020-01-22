713af6f94def4677a4c48dd24192927cc132a433
Szántó Csaba Zsolt <szantocszs@gmail.com>
2020-01-22 15:48:14 +0100

    ssss

---
+++ l2l.Data.Test/CourseRepositoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace l2l.Data.Test
+{
+    public class CourseRepositoryTests
+    {
+        [Fact]
+        public void CourseRepositoryTests_AddedCoursesShouldBeAppearInTheRepositorydot()
+        {
+
+        }
+    }
+}
